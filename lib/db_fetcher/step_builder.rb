cfce3bf5d2f7c70920a0115550217b308cc74f0a
Sergey Gernyak <sergeg1990@gmail.com>
2014-12-20 19:56:33 +0200

    Some doc for building step method

--- lib/db_fetcher/step_builder.rb
+++ lib/db_fetcher/step_builder.rb
@@ -6,6 +6,21 @@ module DbFetcher
       @steps = []
     end
 
+    # Define step for runner
+    #
+    # ==== Attributes
+    #
+    # * +opts+ - options for defining proper step. It may be hash or even instance one of the predefined steps.
+    #
+    # ==== Examples
+    #
+    # builder = DbFetcher::StepBuilder.new
+    # # Passing hash
+    # builder.step local_run: { command: 'rake test' }
+    # # Define step manually
+    # another_step = DbFetcher::StepDefinitions::LocalRun.new(command: 'cd some_project')
+    # # Add defined step to step builder
+    # builder.step another_step
     def step
       @steps << DbFetcher::StepDefinitions::Basic.new
     end
