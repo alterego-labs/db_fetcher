d3504fa9a19cb5d7380f66714d910a5e09c6172b
Sergey Gernyak <sergeg1990@gmail.com>
2014-12-25 02:58:32 +0200

    Passing options to step initialization

--- lib/db_fetcher/step_definitions/basic.rb
+++ lib/db_fetcher/step_definitions/basic.rb
@@ -1,7 +1,11 @@
 module DbFetcher
   module StepDefinitions
     class Basic
-      attr_reader :target_context
+      attr_reader :target_context, :options
+
+      def initialize(options = {})
+        @options = options
+      end
 
       def run
       end
--- test/lib/db_fetcher/step_definitions/basic_test.rb
+++ test/lib/db_fetcher/step_definitions/basic_test.rb
@@ -2,11 +2,17 @@ require_relative '../../../test_helper'
 
 class DbFetcher::StepDefinitions::BasicTest < Minitest::Test
   def setup
-    @step = DbFetcher::StepDefinitions::Basic.new
+    @options = { some_key: :some_val }
+    @step = DbFetcher::StepDefinitions::Basic.new @options
+  end
+
+  def test_initialization_with_options
+    assert_equal @step.options, @options
   end
 
   def test_responsebilities
     assert_respond_to @step, :target_context
     assert_respond_to @step, :run
+    assert_respond_to @step, :options
   end
 end
