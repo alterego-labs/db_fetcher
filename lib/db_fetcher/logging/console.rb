ef479e3ac1c21e3cdb1388f6ecf33469221c642b
Sergey Gernyak <sergeg1990@gmail.com>
2014-12-26 17:32:19 +0200

    Step starting logging

--- lib/db_fetcher/logging/console.rb
+++ lib/db_fetcher/logging/console.rb
@@ -1,8 +1,18 @@
 module DbFetcher
   module Logging
     class Console < Basic
+      def step_start(name)
+        write "  * #{formatted_now} => Currently executing #{name}", :green
+      end
+
       def write(message, color = :white)
         puts message.send(color)
+      end
+
+      private
+
+      def formatted_now
+        DateTime.now.strftime '%e %b %Y %H:%M:%S%p'
       end
     end
   end
--- test/lib/db_fetcher/logging/console_test.rb
+++ test/lib/db_fetcher/logging/console_test.rb
@@ -5,8 +5,19 @@ class DbFetcher::Logging::ConsoleTest < Minitest::Test
     @logger = DbFetcher::Logging::Console.new
   end
 
+  def test_responsebilities
+    %w[write step_start].each do |method|
+      assert_respond_to @logger, method
+    end
+  end
+
   def test_write
     @logger.expects(:puts)
     @logger.write 'some'
+  end
+
+  def test_step_start
+    @logger.expects(:write)
+    @logger.step_start :local_run
   end
 end
